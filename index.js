f1c66fda5bb4480b91e85ec2cf97e5aee0e679ab
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-11-19 16:28:57 +0600

    update code

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const client = new MongoClient(uri, {
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
+  console.log("authHeader", authHeader);
   if (!authHeader) {
     return res.status(401).send({ message: "unauthorization access" });
   }
@@ -50,13 +51,29 @@ async function run() {
       .collection("appointmentOptions");
 
     const bookingCollection = client.db("doctors").collection("bookings");
+    const usersCollection = client.db("doctors").collection("users");
 
-    app.post("/jwt", (req, res) => {
-      const user = req.body;
-      var token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
-      res.send({ token });
+    // app.post("/jwt", (req, res) => {
+    //   const user = req.body;
+    //   var token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1h",
+    //   });
+    //   res.send({ token });
+    // });
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        var token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1h",
+        });
+        console.log(token);
+        return res.send({ accessToken: token });
+      }
+
+      res.status(403).send({ accessToken: "" });
     });
 
     // Get Appointment Options
@@ -143,6 +160,18 @@ async function run() {
      * app.delete('/bookings/:id')
      * */
 
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+      if (email != decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      //console.log("token", req.headers.authorization);
+      const query = { email: email };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       console.log(booking);
@@ -160,6 +189,44 @@ async function run() {
       }
 
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const resut = await usersCollection.find(query).toArray();
+      res.send(resut);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const resut = await usersCollection.insertOne(user);
+      res.send(resut);
+    });
+
+    app.put("/users/admin/:id", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const option = {
+        upsert: true,
+      };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        option
+      );
       res.send(result);
     });
 
